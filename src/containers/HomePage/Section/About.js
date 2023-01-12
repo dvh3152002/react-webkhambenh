a8c3c0c9e53bdee4746b393c4e00e6fba25a7b82
dvh3152002 <dvh31502@gmail.com>
2023-01-12 17:40:16 +0700

    Sử dụng redux thay đổi ngôn ngữ

--- src/containers/HomePage/Section/About.js
+++ src/containers/HomePage/Section/About.js
@@ -12,7 +12,7 @@ class About extends Component {
                 </div>
                 <div className='about-content-section'>
                     <div className='content-left'>
-                        <iframe width="100%" height="400px" src="https://www.youtube.com/embed/147SkAVXEqM?list=PLncHg6Kn2JT6E38Z3kit9Hnif1xC_9VqI" title="#51 Kết Thúc Design Giao Diện Clone BookingCare.vn 4 | React.JS Cho Người Mới Bắt Đầu" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                        <iframe width="100%" height="400px" src="https://www.youtube.com/embed/147SkAVXEqM?list=PLncHg6Kn2JT6E38Z3kit9Hnif1xC_9VqI" title="#51 Kết Thúc Design Giao Diện Clone BookingCare.vn 4 | React.JS Cho Người Mới Bắt Đầu" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
                     </div>
                     <div className='content-right'>
                         <p>Tuần trước em có dịp tham quan biển Phan Thiết. Cảnh nơi đây rất đẹp. Những hàng dừa trải dài xanh ngắt nằm dọc bờ biển. Giữa biển có một bãi cát trắng dẫn thẳng vào các tòa nhà nghỉ của khách du lịch, sau tòa nhà là những dãy núi tim tím. Xa xa trên mặt biển, có những chiếc thuyền chở khách du lịch đi thăm quan. Cảnh vật ở biển Phan Thiết thật yên bình. Em yêu cảnh biển nơi đây, và cũng yêu đất nước Việt Nam của mình.</p>
